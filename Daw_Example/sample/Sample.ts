e284b178d4f64c7a5ad5967f3b6fc55dd070a49d
martymods <50667313+martymods@users.noreply.github.com>
2025-09-26 01:20:56 -0400

    Add files via upload

---
+++ Daw_Example/sample/Sample.ts
@@ -0,0 +1,4 @@
+import {SampleMetaData} from "./SampleMetaData"
+import {UUID} from "@opendaw/lib-std"
+
+export type Sample = { uuid: UUID.String } & SampleMetaData
---
+++ Daw_Example/sample/SampleMetaData.ts
@@ -0,0 +1,7 @@
+export type SampleMetaData = {
+    name: string
+    bpm: number
+    duration: number
+    sample_rate: number
+    cloud?: string
+}
